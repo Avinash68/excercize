a65e88be347231afbf9c3a0691cc6bc581094f23
Avinash Thorat <avinash.thorat@mohartech.com>
2018-04-11 11:21:17 +0530

    Initial commit

---
+++ exercize/projects/ex47/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+config = {
+    'description': 'My Project',
+    'author': 'AWESOMEEEEEEEE',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'mr.awesome@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'Name'}
+setup(**config)
---
+++ exercize/shellscript/hello.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+echo "Hello Avinash"
+echo "This is Me-Avinash (*_-)"
